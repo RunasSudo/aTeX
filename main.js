6486da01477b398894fe4edcab2505cba13dc5f3
RunasSudo <RunasSudo@gmail.com>
2015-12-16 18:00:11 +1030

    Basic framework

--- main.js
+++ main.js
@@ -14,3 +14,140 @@
 //    You should have received a copy of the GNU Affero General Public License
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+class StringReader {
+	constructor(string) {
+		this.string = string;
+		this.ptr = 0;
+	}
+	
+	getPos() {
+		return this.ptr;
+	}
+	
+	hasNext() {
+		return this.ptr < this.string.length;
+	}
+	
+	peek() {
+		if (this.ptr >= this.string.length) {
+			return "EOF";
+		}
+		return this.string[this.ptr];
+	}
+	
+	next() {
+		if (this.ptr >= this.string.length) {
+			return "EOF";
+		}
+		return this.string[this.ptr++];
+	}
+}
+
+class TeXSyntaxError extends Error {
+	constructor(message) {
+		super(message);
+		this.name = "TeXSyntaxError";
+		this.message = message;
+	}
+}
+
+class TeXParser {
+	constructor(reader) {
+		this.reader = reader;
+	}
+	
+	parseTeX() {
+		let buffer = "";
+		try {
+			while (this.reader.hasNext()) {
+				let out = "";
+				if ((out = this.parseMacro())
+				 || (out = this.parseText())
+				) {
+					buffer += out;
+				} else {
+					throw new TeXSyntaxError("Unknown symbol " + this.reader.peek());
+				}
+			}
+		} catch (ex) {
+			buffer += '<span class="tex-error">' + ex.name + ': ' + ex.message + ' near ' + this.reader.getPos() + '</span>';
+			console.log(ex.name + ": " + ex.message);
+			console.log(ex.stack);
+		}
+		return buffer;
+	}
+	
+	escapeRegex(regex) {
+		if (typeof(regex) === "string") {
+			return RegExp(regex.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&"));
+		} else {
+			return regex;
+		}
+	}
+	
+	accept(regex, strict = false) {
+		if (this.reader.peek().match(this.escapeRegex(regex))) {
+			return this.reader.next();
+		} else {
+			if (strict) {
+				throw new TeXSyntaxError("Expecting " + regex + ", got " + this.reader.peek());
+			}
+		}
+	}
+	
+	readString(regex) {
+		let buffer = "";
+		if (!this.reader.peek().match(regex)) {
+			throw new TeXSyntaxError("Expecting " + regex + ", got " + this.reader.peek());
+		}
+		
+		while (this.reader.hasNext() && this.reader.peek().match(regex)) {
+			buffer += this.reader.next();
+		}
+		
+		return buffer;
+	}
+	
+	parseText() {
+		let out;
+		if ((out = this.accept(/[^#\$%\^&_\{\}~\\]/))) {
+			return out;
+		}
+	}
+	
+	parseGroup() {
+		let group = "";
+		if (!this.accept("{")) {
+			throw new TeXSyntaxError("Expecting {, got " + this.reader.peek());
+		}
+		
+		// Go through characters, find nested groups and exit on un-nested }
+		while (this.reader.hasNext()) {
+			if (this.reader.peek() === "{") {
+				group += "{" + this.parseGroup() + "}";
+			} else if (this.reader.peek() === "}") {
+				this.accept("}");
+				return group;
+			} else {
+				group += this.reader.next();
+			}
+		}
+		
+		throw new TeXSyntaxError("Expecting }, got EOF");
+	}
+	
+	parseMacro() {
+		if (this.accept("\\")) {
+			let macro = this.readString(/[a-zA-Z]/);
+			let starred = this.accept("*");
+			let args = [];
+			while (this.reader.peek() === "{") {
+				args.push(this.parseGroup());
+			}
+			return "MACRO " + macro + "," + args; //TODO
+		}
+		return false;
+	}
+}
+
+new TeXParser(new StringReader("Hello World!")).parseTeX();
